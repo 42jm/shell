bcb8832ad6678ee8ae75411d5135f9b50d9b5da8
42jm <dzenza8@gmail.com>
2020-09-30 19:01:52 +0200

    removed_testputs

--- libft/ft_lstdup.c
+++ libft/ft_lstdup.c
@@ -17,20 +17,15 @@ t_list	*ft_lstdup(t_list *lst)
 	t_list	*dup;
 	t_list	*dup_head;
 
-	ft_putendl("\t0");
 	if (!lst)
 		return (NULL);
-	ft_putendl("\t1");
 	dup = ft_lstnew(lst->content, lst->content_size);
 	dup_head = dup;
-	ft_putendl("\t2");
 	while (lst->next)
 	{
-		ft_putendl("\t2.1");
 		lst = lst->next;
 		dup->next = ft_lstnew(lst->content, lst->content_size);
 		dup = dup->next;
 	}
-	ft_putendl("\t3");
 	return (dup_head);
 }
